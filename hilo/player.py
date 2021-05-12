d7d9106a8e8933c522b62693bd3c549b2bb70d93
Stephanie Guthrie <srguthrie257@gmail.com>
2021-05-12 13:14:17 -0600

    Added player.py, need to work on it some more

---
+++ hilo/player.py
@@ -0,0 +1,24 @@
+class Dealer:
+
+    def __init__(self):
+        #dealer attributes
+
+        self.cards = []
+
+    def show_card(self):
+
+
+
+
+
+class Player:
+
+    def __init__(self):
+        #player attributes
+
+        self.points = 300
+
+    def make_guess(self):
+        
+
+    def add_or_sub_points(self):

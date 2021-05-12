ef1fdf1f9ceb5f31871c893e3a9e68fe509d6cf0
Morgan Mann <momomann4@gmail.com>
2021-05-12 11:43:08 -0600

    Wrote outline for game

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -1,1 +1,26 @@
-# TODO: Add entry point code here
+# class director
+
+# def start_game
+    #Starts game and keeps game running
+
+# def get_cards
+    #gets next two cards (shuffles deck)
+
+#def do_updates
+    #updates score
+
+#def do_outputs
+    #outputs info such as card and score
+
+
+
+# class deck
+
+#def can_play
+    #checks if score is greater than 0
+
+#def get_ponts
+    #calculates total points
+
+#def shuffle_deck
+    # generates new cards to compare

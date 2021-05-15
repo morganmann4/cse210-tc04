9091d71650578cb7427a05074a838b6ac13f29b5
Stephanie Guthrie <srguthrie257@gmail.com>
2021-05-14 18:59:07 -0600

    added test code

--- hilo/w4pytest.py
+++ hilo/w4pytest.py
@@ -1,9 +1,22 @@
-import pytest
- from game.player import Player
+import pytest 
+
+from dealer import is_guess_correct
+from player import add_or_sub_points
 
 #check to see if the functions work
 
-def validate_Check_guess():
+def validate_is_guess_correct():
     
-    assert
+    assert is_guess_correct(h, 7, 10) == True
+    assert is_guess_correct(h, 10, 7) == False
+    assert is_guess_correct(l, 10, 7) == True
+    assert is_guess_correct(l, 7, 10) == False
+
+def validate_add_or_sub_points():
+    points = 100
+    assert add_or_sub_points(True) == points + 100
+    assert add_or_sub_points(False) == points - 75
+    
+
+pytest.main(["-v", "--tb=no", "test_w4pytest.py"])
 

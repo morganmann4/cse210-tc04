eff9c0123916b4e6ac17d43017e61d3141114060
Stephanie Guthrie <srguthrie257@gmail.com>
2021-05-14 19:10:42 -0600

    test code for two methods

--- hilo/w4pytest.py
+++ hilo/w4pytest.py
@@ -1,22 +1,27 @@
 import pytest 
 
-from dealer import is_guess_correct
-from player import add_or_sub_points
+from dealer import Dealer
+from player import Player
 
 #check to see if the functions work
+dealer = Dealer()
+player = Player()
 
-def validate_is_guess_correct():
+def test_is_guess_correct():
+    #tests if given certain parameters, the right booleans are returned
+    assert dealer.is_guess_correct('h', 7, 10) == True
+    assert dealer.is_guess_correct('h', 10, 7) == False
+    assert dealer.is_guess_correct('l', 10, 7) == True
+    assert dealer.is_guess_correct('l', 7, 10) == False
+
+def test_add_or_sub_points():
+    #tests to see if the points are actually added or subtracted
+    testPoints = 300
+    player.points = testPoints
+    assert player.add_or_sub_points(True) == testPoints + 100
+    player.points = testPoints
+    assert player.add_or_sub_points(False) == testPoints - 75
     
-    assert is_guess_correct(h, 7, 10) == True
-    assert is_guess_correct(h, 10, 7) == False
-    assert is_guess_correct(l, 10, 7) == True
-    assert is_guess_correct(l, 7, 10) == False
 
-def validate_add_or_sub_points():
-    points = 100
-    assert add_or_sub_points(True) == points + 100
-    assert add_or_sub_points(False) == points - 75
-    
-
-pytest.main(["-v", "--tb=no", "test_w4pytest.py"])
+pytest.main(["-v", "--tb=no", "w4pytest.py"])
 
